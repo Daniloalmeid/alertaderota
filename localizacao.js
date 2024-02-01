81609cceb88b51df9d1db974f11f4d8d990a651d
Danilo Almeida <danilotecteatro@yahoo.com.br>
2024-02-01 14:29:44 -0300

    atu

--- localizacao.js
+++ localizacao.js
@@ -9,7 +9,7 @@ function initMap() {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         alert("Geolocation is not supported by this browser.");
     }
@@ -67,6 +67,22 @@ function addMarker(place) {
     });
 }
 
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
 
 
 //   AIzaSyCauBvST5zOEV-dZlQJ80318LJbC0Dz3Tk    //
